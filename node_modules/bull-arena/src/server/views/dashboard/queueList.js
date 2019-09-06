7255c5b879debe33e590983be859b4d4dcd88b9c
Tanapon <tanapon@renthub.in.th>
2019-09-06 11:16:26 +0700

    remove node_modules

--- node_modules/bull-arena/src/server/views/dashboard/queueList.js
+++
@@ -1,9 +0,0 @@
-function handler(req, res) {
-  const {Queues} = req.app.locals;
-  const queues = Queues.list();
-  const basePath = req.baseUrl;
-
-  return res.render('dashboard/templates/queueList', { basePath, queues });
-}
-
-module.exports = handler;
