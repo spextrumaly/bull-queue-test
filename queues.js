e3dec090a3f73fe653b64051f3d2a453ff7187f7
Tanapon <tanapon@renthub.in.th>
2019-09-09 11:45:00 +0700

    add name in job

--- queues.js
+++ queues.js
@@ -3,15 +3,3 @@ export const TEST_QUEUE = "TEST_QUEUE";
 export const queues = {
   [TEST_QUEUE]: new Queue(TEST_QUEUE, "0.0.0.0")
 };
-
-const data = {
-  key: "testKey"
-};
-
-const options = {
-  repeat: {
-    limit: 10,
-    every: 10000
-  },
-  attempts: 3
-};
--- worker.js
+++ worker.js
@@ -1,7 +1,20 @@
 import { processorTest } from "./processors";
 import { queues } from "./queues";
-queues.TEST_QUEUE.add(data, options);
-queues.TEST_QUEUE.process(processorTest);
+
+const data = {
+  key: "bullTestRepeat"
+};
+
+const options = {
+  repeat: {
+    limit: 10,
+    every: 10000
+  },
+  attempts: 3
+};
+
+queues.TEST_QUEUE.add("test", data, options);
+queues.TEST_QUEUE.process("test", processorTest);
 
 queues.TEST_QUEUE.on("completed", (job, result) => {
   console.log("result::", result);
